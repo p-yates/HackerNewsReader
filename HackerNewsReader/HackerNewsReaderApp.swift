fcdade184e4875569e284b2cf4b025c02e0438e6
p-yates <148437369+p-yates@users.noreply.github.com>
2023-11-24 10:37:45 +0100

    Initial Commit

---
+++ HackerNewsReader/HackerNewsReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsReaderApp.swift
+//  HackerNewsReader
+//
+//  Created by Paul Y on 2023-11-24.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
